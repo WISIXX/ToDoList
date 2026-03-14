a4e6b5455abb4aaf1a7ebb4ee84a3345566a9f2f
WISIXX <wisix.le.pro@gmail.com>
2026-03-15 00:53:18 +0100

    Ajoutez des fichiers projet.

---
+++ ToDoList/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace ToDoList
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            checkedListBox1 = new CheckedListBox();
+            button1 = new Button();
+            checkBox1 = new CheckBox();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Cursor = Cursors.IBeam;
+            textBox1.Location = new Point(12, 53);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(269, 23);
+            textBox1.TabIndex = 0;
+            textBox1.Enter += textBox1_Enter;
+            textBox1.KeyDown += textBox1_KeyDown;
+            // 
+            // checkedListBox1
+            // 
+            checkedListBox1.FormattingEnabled = true;
+            checkedListBox1.Location = new Point(371, 53);
+            checkedListBox1.Name = "checkedListBox1";
+            checkedListBox1.Size = new Size(417, 328);
+            checkedListBox1.TabIndex = 1;
+            checkedListBox1.SelectedIndexChanged += checkedListBox1_SelectedIndexChanged;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.DarkRed;
+            button1.Cursor = Cursors.Hand;
+            button1.ForeColor = SystemColors.Control;
+            button1.Location = new Point(12, 102);
+            button1.Name = "button1";
+            button1.Size = new Size(108, 31);
+            button1.TabIndex = 2;
+            button1.Text = "Remove";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(12, 419);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(81, 19);
+            checkBox1.TabIndex = 3;
+            checkBox1.Text = "DarkMode";
+            checkBox1.UseVisualStyleBackColor = true;
+            checkBox1.CheckedChanged += checkBox1_CheckedChanged_1;
+            checkBox1.Click += checkBox1_Click;
+            // 
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.WhiteSmoke;
+            ClientSize = new Size(800, 450);
+            Controls.Add(checkBox1);
+            Controls.Add(button1);
+            Controls.Add(checkedListBox1);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private CheckedListBox checkedListBox1;
+        private Button button1;
+        private CheckBox checkBox1;
+    }
+}
---
+++ ToDoList/Form1.cs
@@ -0,0 +1,104 @@
+using Microsoft.VisualBasic.Devices;
+
+namespace ToDoList
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void textBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+
+
+            if (e.KeyCode == Keys.Enter)
+            {
+
+                e.SuppressKeyPress = true;
+
+                string test = textBox1.Text;
+
+                if (!string.IsNullOrEmpty(test))
+                {
+
+
+
+                    textBox1.Clear();
+
+                    checkedListBox1.Items.Add(test);
+                }
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (checkedListBox1.Items.Count != 0)
+            {
+
+                checkedListBox1.Items.RemoveAt(checkedListBox1.Items.Count - 1);
+            }
+
+
+        }
+
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_Click(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                this.BackColor = Color.FromArgb(6, 19, 20);
+                textBox1.BackColor = Color.FromArgb(6, 19, 20);
+                checkedListBox1.BackColor = Color.FromArgb(6, 19, 20);
+
+                checkBox1.ForeColor = Color.FromArgb(252, 252, 252);
+                textBox1.ForeColor = Color.White;
+                checkedListBox1.ForeColor = Color.White;
+
+            }
+            else if (!checkBox1.Checked)
+            {
+                this.BackColor = Color.FromArgb(200, 237, 239);
+                textBox1.BackColor = Color.White;
+                checkedListBox1.BackColor = Color.White;
+
+                checkBox1.ForeColor = Color.Black;
+                textBox1.ForeColor = Color.Black;
+                checkedListBox1.ForeColor = Color.Black;
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.BackColor = Color.FromArgb(200, 237, 239);
+        }
+
+        private void checkBox1_CheckedChanged_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
